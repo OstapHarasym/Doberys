39a7bc7d2087435602a7093e9dc02c244dc24c17
OstapHarasym <70802246+OstapHarasym@users.noreply.github.com>
2021-03-16 17:50:53 +0200

    first commit

---
+++ Doberys_game/MyForm.cpp
@@ -0,0 +1,11 @@
+﻿#include "MyForm.h"
+#include <Windows.h>
+//using namespace Doberysgame; 
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) 
+{
+	Doberysgame::Application::EnableVisualStyles();
+	Doberysgame::Application::SetCompatibleTextRenderingDefault(false);
+	Doberysgame::Application::Run(gcnew Doberysgame::MyForm);
+	return 0;
+}
